22d55cb65d333b7400538fdb8dc3e6b4fbe9a1ec
kiteo111 <josphatinde@gmail.com>
2024-10-30 14:11:10 -0700

    first commit

---
+++ SimpleCalculator.java
@@ -0,0 +1,17 @@
+public class SimpleCalculator{
+    public static void multiply(){
+        int num1 = 6;
+        int num2 = 10;
+        int answer = num1*num2;
+
+    
+    System.out.println(answer);
+
+
+    }
+    public static void main(String[] args){
+      SimpleCalculator jos = new SimpleCalculator();
+      jos.multiply();  
+
+    }
+}
